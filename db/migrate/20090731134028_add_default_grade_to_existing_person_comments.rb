7f42dc9e8319c192d50528701f34ecabec7af771
Michael Dawson <email.michaeldawson@gmail.com>
2015-12-21 15:58:57 +1030

    Moar data stuff

--- db/migrate/20090731134028_add_default_grade_to_existing_person_comments.rb
+++ db/migrate/20090731134028_add_default_grade_to_existing_person_comments.rb
@@ -1,10 +1,10 @@
 class AddDefaultGradeToExistingPersonComments < ActiveRecord::Migration
   def self.up
-    PersonComment.all.each do |comment|
-      unless comment.grade
-        comment.update_attribute(:grade, 0.5)
-      end  
-    end  
+    # PersonComment.all.each do |comment|
+    #   unless comment.grade
+    #     comment.update_attribute(:grade, 0.5)
+    #   end  
+    # end  
   end
 
   def self.down
