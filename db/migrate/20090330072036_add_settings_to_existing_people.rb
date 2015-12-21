2d2810896cbfbe73ede02689c55196b774f45032
Michael Dawson <email.michaeldawson@gmail.com>
2015-12-21 14:56:00 +1030

    Wipe out problematic migration

--- db/migrate/20090330072036_add_settings_to_existing_people.rb
+++ db/migrate/20090330072036_add_settings_to_existing_people.rb
@@ -1,13 +1,13 @@
 class AddSettingsToExistingPeople < ActiveRecord::Migration
   def self.up
-    Person.find(:all).each do |person| 
-      person.settings = Settings.create
-    end  
+    # Person.find(:all).each do |person| 
+    #   person.settings = Settings.create
+    # end  
   end
 
   def self.down
-    Person.find(:all).each do |person| 
-      person.settings = nil
-    end
+    # Person.find(:all).each do |person| 
+    #   person.settings = nil
+    # end
   end
 end
